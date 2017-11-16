cf8ff42a27d50893dd8cf88b255bcf69f8153aa3
mazhida <138766778@qq.com>
2017-11-16 10:14:52 +0800

    11/16 index 3.0

--- application/back/view/article/index.php
+++ application/back/view/article/index.php
@@ -89,7 +89,7 @@
                 </div>
 				<div class="col-xs-">
                     <a href="{:url('edit')}?id={$row_->id}"><button class="btn btn-success btn-xs edit_" >修改</button></a>
-					<?php if($row_->id!==1){?>
+					<?php if($row_->id!=1){?>
                     <button class="btn btn-danger btn-xs del_cate" data-toggle="modal"
 							data-target="#deleteSource" data-id="<?= $row_['id'] ?>" onclick="del_(this)"> 删除
                     </button>
